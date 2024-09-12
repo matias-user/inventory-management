423f890eb0292054117a16895d4e806bb76e70db
Matías <matias.abarca2018@gmail.com>
2024-09-12 20:08:58 -0300

    Se agregan modelos productos, precio y descuento

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField( max_length=255, blank=False )
+    quantity = models.IntegerField( default=0 )
+    sku = models.CharField( max_length=255 )
+
+
+class Price(models.Model):
+    product = models.ForeignKey( Product, on_delete=models.CASCADE )
+    amount = models.IntegerField( default=0 )
+
+
+class discount(models.Model):
+    product = models.ForeignKey( Product, on_delete=models.CASCADE )
+    porcentage = models.FloatField(default=0 )
+    amount = models.IntegerField(default=0)
