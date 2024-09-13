5cc2cb61ee59ad680ac74d863dfe279321ddab2d
Matías <matias.abarca2018@gmail.com>
2024-09-12 21:48:54 -0300

    Se agrega template crear producto, la clase form y el controlador para agregar a la tabla

--- inventory/urls.py
+++ inventory/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name= "inventory"
 urlpatterns = [
-    path('', views.index, name='index' )
+    path('', views.index, name='index' ),
+    path('create/', views.createProduct, name='create-product' ),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from .forms import ProductForm
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def index(request):
+
     return render(request, 'inventory/home.html' )
+
+
+def createProduct(request):
+
+    if request.method == 'POST':
+
+        form = ProductForm( request.POST )
+
+        if form.is_valid():
+            print( request.POST )
+            return HttpResponseRedirect("Gracias!")
+    else:
+        form = ProductForm()
+
+
+    return render(request, 'inventory/createProduct.html',{"form":form})
