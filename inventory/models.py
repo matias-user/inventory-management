f990ad196944d40c3bee20b5c057f4bc2ca3ab35
Matías <matias.abarca2018@gmail.com>
2024-09-22 15:55:10 -0300

    Pequeño cambio en edit.html

--- inventory/models.py
+++ inventory/models.py
@@ -4,15 +4,27 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField( max_length=255, blank=False )
     quantity = models.IntegerField( default=0 )
-    sku = models.CharField( max_length=255 )
-
+    sku = models.CharField( max_length=255, unique=True )
+    brand = models.CharField(max_length=255)
+    created_date = models.DateTimeField( auto_now_add=True )
 
 class Price(models.Model):
     product = models.ForeignKey( Product, on_delete=models.CASCADE )
     amount = models.IntegerField( default=0 )
 
-
 class discount(models.Model):
     product = models.ForeignKey( Product, on_delete=models.CASCADE )
     porcentage = models.FloatField(default=0 )
     amount = models.IntegerField(default=0)
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+class CategoryProduct(models.Model):
+    product_id = models.ForeignKey( Product, on_delete=models.CASCADE )
+    category_id = models.ForeignKey( Category, on_delete=models.CASCADE )
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=255)
+    phone = models.IntegerField(default=0)
+    contact_email = models.IntegerField( null=False )
