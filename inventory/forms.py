90520f086a79e267cdd6e20ed92bd6c4bbe8522d
Matías <matias.abarca2018@gmail.com>
2024-09-23 14:38:21 -0300

    Se termina función de js para traspasar valores de input de modal a input de djangoform

--- inventory/forms.py
+++ inventory/forms.py
@@ -8,7 +8,7 @@ class ProductForm(forms.ModelForm):
     quantity = forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control'}))
     sku = forms.CharField( widget=forms.TextInput(attrs={'class':'form-control'}) )
     brand = forms.CharField( widget=forms.TextInput(attrs={'class':'form-control'}) )
-    description = forms.CharField( widget=forms.Textarea(attrs={'class':'form-control'}) )
+    description = forms.CharField( widget=forms.Textarea(attrs={'class':'form-control form-control-sm','rows':'3'}) )
     height = forms.FloatField( widget=forms.HiddenInput() )
     weight = forms.FloatField( widget=forms.HiddenInput() )
     color = forms.CharField(widget=forms.HiddenInput())
