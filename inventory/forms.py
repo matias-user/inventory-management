ff6949670576d47452f409c5fda1c229df1df318
Matías <matias.abarca2018@gmail.com>
2024-09-23 22:50:58 -0300

    Se ajusta el form crear producto el bootstrap y se agrega un css adicional en carpeta static

--- inventory/forms.py
+++ inventory/forms.py
@@ -5,10 +5,10 @@ from django import forms
 class ProductForm(forms.ModelForm):
     price = forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control'}))
     name = forms.CharField( widget=forms.TextInput(attrs={'class':'form-control'}) )
-    quantity = forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control'}))
+    description = forms.CharField( widget=forms.Textarea(attrs={'class':'form-control form-control-sm','rows':'3'}) )
     sku = forms.CharField( widget=forms.TextInput(attrs={'class':'form-control'}) )
     brand = forms.CharField( widget=forms.TextInput(attrs={'class':'form-control'}) )
-    description = forms.CharField( widget=forms.Textarea(attrs={'class':'form-control form-control-sm','rows':'3'}) )
+    quantity = forms.IntegerField(widget=forms.NumberInput(attrs={'class':'form-control'}))
     height = forms.FloatField( widget=forms.HiddenInput() )
     width = forms.FloatField( widget=forms.HiddenInput() )
     color = forms.CharField(widget=forms.HiddenInput())
